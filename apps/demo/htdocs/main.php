9a112d4dfbef6908db9997fcac4e249cdb250ff6
JYL123 <li474412210@gmail.com>
2018-03-20 23:34:43 +0800

    add a nice background css and main.php

---
+++ apps/demo/htdocs/main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>  
+<head>
+  <title>UPDATE PostgreSQL data with PHP</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <style>li {list-style: none;}
+  .footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: gray;
+   color: white;
+   text-align: center;
+  } 
+  #nav {
+  background-color: #C08374;
+  border: 1px solid #A76358;
+  text-align: center;
+  }
+  ul {
+  list-style: none;
+  display: inline-block;
+  }
+  ul li {
+  float: left;
+  margin: 0 20px;
+  }
+  ul li a {
+  color: white;
+  }
+  </style>
+  <link rel="stylesheet" href="background.css">
+</head>
+<body>
+ <div id="nav">
+  <ul>
+    <h2>Welcome to Car Pooling</h2>
+  </ul>
+  </div>
+  <a href="userLogin.php" style={{'textDecoration': 'none', 'color': 'black'}}>Sign in/Log in as a User</a><break></break>
+  <a href="testpage.php" style={{'textDecoration': 'none', 'color': 'black'}}>Sign in/Log in as a Admin</a>
+  <div class="footer">
+    <p>Privacy and Security Terms of Use Learn More About Cookies</p>
+    <p>© 2018 Car Pooling</p>            
+  </div>
+</body>
+</html>
