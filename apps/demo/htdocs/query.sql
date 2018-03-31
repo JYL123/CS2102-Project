4e64129efac48f7577edea62dee0df62a35d857c
zhang shuoyang <>
2018-03-31 16:16:29 +0800

    modify query.sql

--- apps/demo/htdocs/query.sql
+++ apps/demo/htdocs/query.sql
@@ -5,28 +5,33 @@ SELECT username FROM users WHERE icnum = '$_POST[icnum]';
 SELECT adid FROM advertisements ORDER BY adid DESC LIMIT 1;
 SELECT * FROM advertisements WHERE EXISTS (SELECT 1 FROM advertise WHERE advertisements.adid = advertise.adid);
 SELECT * FROM bid WHERE adid = $_POST[adid] AND icnum = '$_POST[icnum]';
-SELECT icnum, firstName, lastName FROM users where username = '$_POST[username]' and userpassword = '$_POST[userpassword]';
+SELECT icnum FROM users where username = '$_POST[username]' and userpassword = '$_POST[userpassword]';
 BEGIN;
 INSERT INTO advertisements (origin, destination, doa) VALUES ('$_POST[origin]', '$_POST[destination]', '$_POST[doa]');
 INSERT INTO advertise (icnum, adid) SELECT '$_SESSION[icnum]', adid FROM advertisements ORDER BY adid DESC LIMIT 1;
 COMMIT;
 
 -- insert
-INSERT INTO users VALUES ('$_POST[username]', '$_POST[userpassword]', '$_POST[icnum]',  '$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[phonenum]';
 INSERT INTO administrators (username, userpassword, icnum, firstname, lastname, email, phonenum) VALUES ('$_POST[username]', '$_POST[userpassword]', '$_POST[icnum]', '$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[phonenum]');
 INSERT INTO cars (platenum, models, numseats) VALUES ('$_POST[platenum]', '$_POST[models]', '$_POST[numseats]');
 INSERT INTO advertisements (origin, destination, doa) VALUES ('$_POST[origin]', '$_POST[destination]', '$_POST[doa]');
 INSERT INTO advertise (icnum, adid) VALUES ('$_POST[icnum]','$row[adid]');
 INSERT INTO bid (adid, icnum, bidpoints) VALUES ($_POST[adid],'$_POST[icnum]', 1);
 INSERT INTO users (username, userpassword, icnum, firstname, lastname, email, phonenum) VALUES ('$_POST[username]', '$_POST[userpassword]', '$_POST[icnum]', '$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[phonenum]');
-INSERT INTO drive(platenum, icnum) VALUES ('$_POST[platenum]', '$_SESSION['icnum']');
+INSERT INTO drive(plateNum, ICNum) VALUES ('$_POST[platenum]', '$_POST[icnum]');
 
 -- delete
 DELETE FROM users WHERE icnum = '$_POST[icnum]';
+DELETE FROM drive WHERE plateNum = '$_POST[platenum]' and icnum='$_POST[icnum]';
+DELETE FROM advertise WHERE adid='$_POST[adid]' and icnum='$_POST[icnum]';
+DELETE FROM bid WHERE adid='_POST[adid]' and icnum='$_POST[icnum]';
+DELETE FROM cars WHERE plateNum='$_POST[platenum]';
+DELETE FROM advertisements WHERE adid='$_POST[adid]';
+
 
 
 -- For displaying bid points of each advertisement. This is to be viewed by administrator.
-SELECT DISTINCT *
+SELECT DISTINCT * 
 FROM (
 	SELECT adid, count(bidpoints) as points
 	FROM bid
@@ -34,8 +39,8 @@ FROM (
 ) AS combined natural join advertisements
 ORDER BY points DESC;
 
--- For displaying ads that are expired. The ads which were posted 14 dayds ago is expired.
-SELECT DISTINCT *
+-- For displaying ads that are expired. The ads which were posted 14 dayds ago is expired. 
+SELECT DISTINCT * 
 FROM (
 	SELECT adid, count(bidpoints) as points
 	FROM bid
@@ -44,12 +49,33 @@ FROM (
 WHERE CURRENT_TIMESTAMP - doa > '14' ;
 
 -- Most popular ad of the week
-SELECT DISTINCT *
+SELECT DISTINCT * 
 FROM (
 	SELECT adid, count(bidpoints) as points
 	FROM bid
     GROUP BY adid
 ) AS combined natural join advertisements
-WHERE CURRENT_TIMESTAMP - doa <= '7'
+WHERE CURRENT_TIMESTAMP - doa <= '7' 
 ORDER BY points DESC
 LIMIT 1;
+
+CREATE OR REPLACE FUNCTION incBid(IC VARCHAR, ad INTEGER, val NUMERIC)
+RETURNS BOOLEAN AS
+'BEGIN
+UPDATE bid
+SET bidPoints=bidPoints+val
+WHERE ICNUM=IC and adID=ad;
+RETURN TRUE;
+END;'
+LANGUAGE PLPGSQL;
+
+
+CREATE OR REPLACE FUNCTION selBidder(IC VARCHAR)
+RETURNS BOOLEAN AS $$
+BEGIN
+UPDATE bid
+SET status='Selected'
+WHERE icnum=IC;
+RETURN TRUE;
+END; $$
+LANGUAGE PLPGSQL;
